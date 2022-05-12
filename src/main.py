6ae834be9c853ee16112a6e73356b0bd02ca6b80
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-12 21:35:05 +0430

    simple dice game is done

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import random
 
 screen = turtle.Screen()
 screen.bgcolor('lightgreen')
@@ -10,7 +11,6 @@ player_two = turtle.Turtle()
 finish_line = turtle.Turtle()
 dice = turtle.Turtle()
 
-
 player_one.penup()
 player_two.penup()
 finish_line.penup()
@@ -19,28 +19,42 @@ dice.penup()
 player_one.color('blue')
 player_two.color('red')
 finish_line.color('black')
-dice.color('purple')
+dice.color('black')
 
 player_one.shape('turtle')
 player_two.shape('turtle')
 dice.shape('circle')
-player_one.shapesize(2, 2, 1)
-player_two.shapesize(2, 2, 1)
+player_one.shapesize(3, 3, 1)
+player_two.shapesize(3, 3, 1)
 finish_line.shapesize(60, 0.1, 10)
-dice.shapesize(2,2,1)
+dice.shapesize(2, 2, 1)
 
 player_one.goto(-300, 200)
 player_two.goto(-300, -200)
 finish_line.forward(350)
-finish_line.write('Finish Line!', align='right', font=20)
-dice.write('Click the Dice!   ',font=30,align='right')
+finish_line.write('Finish Line!    ', align='right', font=20)
+dice.write('Click the Dice!   ', font=30, align='right')
+
+dice_nums = [1, 2, 3, 4, 5, 6]
+
+turn = True
+
+player_one.pendown()
+player_two.pendown()
 
 
 def diceClick(x, y):
     dice.color('white')
     time.sleep(0.1)
-    dice.color('purple')
+    dice.color('black')
+
+    dice_num1 = random.choice(dice_nums)
+    dice_num2 = random.choice(dice_nums)
+
+    player_one.forward(5 * dice_num1)
+    player_two.forward(5 * dice_num2)
 
 
 dice.onclick(diceClick)
+
 turtle.done()
